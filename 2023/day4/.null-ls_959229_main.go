84e1bbb12661f69b1263aa75d4db15c0f1967b20
0xGrizzly <gmcguir2@students.kennesaw.edu>
2023-12-04 20:09:46 -0500

    day 4 work

---
+++ 2023/day4/.null-ls_959229_main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "bufio"
+)
+
+
+func main () {
+
+  file, err := os.Open("input.txt")
+  if err != nil {
+    fmt.Println("Error opening the file", err)
+  }
+
+  scanner := bufio.NewScanner(file)
+
+  for scanner.Scan() {
+    line := scanner.Text()
+
+  }
+
+
+}
