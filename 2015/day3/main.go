339a55c0a3fe35f14712a918f6d313960501b288
0xGrizzly <gmcguir2@students.kennesaw.edu>
2023-12-24 11:37:01 -0500

    gold star solved

--- 2015/day3/main.go
+++ 2015/day3/main.go
@@ -6,15 +6,48 @@ import (
   "io"
 )
 
+type Position struct {
+  x,y int
+}
+
 func main () {
-  file, err := os.OpenFile("input.txt");
+  file, err := os.Open("input.txt");
   if err != nil {
     fmt.Println("Error opening file", err)
     return
   }
   
+  directions, err := io.ReadAll(file)
+  if err != nil {
+    fmt.Println("Error reading file", err)
+    return
+  }
 
+  visitedHouses := make(map[string]bool)
+  santaPosition := Position{0,0}
+  visitedHouses[santaPosition.toKey()] = true
+  
+  for _, direction := range directions {
+    switch direction {
+    case '^':
+      santaPosition.y++
+    case 'v':
+      santaPosition.y--
+    case '>':
+      santaPosition.x++
+    case '<':
+      santaPosition.x--
+    }
+    visitedHouses[santaPosition.toKey()] = true
+  }
 
+  numberofHouses := len(visitedHouses)
+  fmt.Printf("Number of houses that were visited at least once %v", numberofHouses)
+  
+}
+
+func (p Position) toKey() string {
+  return fmt.Sprintf("%d,%d", p.x, p.y)
 }
 
 
