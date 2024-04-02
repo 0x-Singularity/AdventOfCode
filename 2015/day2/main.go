f6dc00a1dc5d552dd8121d252eecb6df376bbb5e
0xGrizzly <gmcguir2@students.kennesaw.edu>
2024-04-02 15:34:30 -0400

    entire problem solved, both gold stars

--- 2015/day2/main.go
+++ 2015/day2/main.go
@@ -30,6 +30,24 @@ func findSmallestArea(length, width, height float64) float64 {
   return smallestArea
 }
 
+func findSmallestPerimeter(length, width, height float64) float64{
+  perimeter1 := 2 * (length + width)
+  perimeter2 := 2 * (width + height)
+  perimeter3 := 2 * (height + length)
+
+  smallestPerimeter := perimeter1
+  if perimeter2 < smallestPerimeter{
+    smallestPerimeter = perimeter2
+  }
+  if perimeter3 < smallestPerimeter {
+    smallestPerimeter = perimeter3
+  }
+  return smallestPerimeter
+}
+
+func findVolume(length, width, height float64) float64 {
+  return length * width * height
+}
 
 func main() {
 
@@ -57,11 +75,15 @@ func main() {
       fmt.Println("Error parsing the height", err)
     }
 
-    surfaceArea := findSurfaceArea(length, width, height)
+    // Needed for part 1 gold star
+    //surfaceArea := findSurfaceArea(length, width, height)
+    //smallestArea := findSmallestArea(length, width, height)
+    //total += surfaceArea + smallestArea
 
-    smallestArea := findSmallestArea(length, width, height)
-   
-    total += surfaceArea + smallestArea
+    smallestPerimeter := findSmallestPerimeter(length, width, height)
+    volume := findVolume(length, width, height)
+    total += smallestPerimeter + volume
+    
   }
   fmt.Printf("total %f", total)
 }
