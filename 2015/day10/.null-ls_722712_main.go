3a680834cacfdd6d9757970e36e2d53edd035502
0x-Singularity <gmcguir2@students.kennesaw.edu>
2024-04-06 09:36:25 -0400

    working on solution for one star

---
+++ 2015/day10/.null-ls_722712_main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+)
+
+func calculate(input string) string {
+  var result string
+  count := 1 
+  for i := 0; i < len(input); i++{
+    // checks if i+1 is within the bounds of input and if they are the same character
+    if i+1 < len(input) && input[i] == input[i+1]{
+      count++
+    }else{
+      result += fmt.Sprintf("%d%c", count, input[i])
+      count = 1
+    }
+  }
+  return result 
+}
+
+func main () {
+  input := "1321131112"
+  for i:= 0; i < 40; i++{
+    input = calculate(input)
+  }
+  fmt.Println(len(input))
+}
