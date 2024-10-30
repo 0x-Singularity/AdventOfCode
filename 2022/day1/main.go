5133a444ae57eeb416c769bcdd0b44d9f5452705
0xSingularity <gmcguir2@kennesaw.students.edu>
2024-10-30 18:38:52 -0400

    gold start for day1

--- 2022/day1/main.go
+++ 2022/day1/main.go
@@ -1,50 +1,48 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "bufio"
-  "strconv"
+    "bufio"
+    "fmt"
+    "os"
+    "sort"
+    "strconv"
 )
 
 func main() {
-  file,err := os.Open("input.txt")
-  if err != nil{
-    fmt.Println("Error opening the file", err)
-  }
-  defer file.Close()
-
-  scanner := bufio.NewScanner(file)
-
-  currentSum := 0
-  maxCalories := 0
-
-  for scanner.Scan() {
-    line := scanner.Text()
-    if line == ""{
-      if currentSum > maxCalories {
-        maxCalories = currentSum
-      }
-      currentSum = 0
-    } else {
-      calories, err := strconv.Atoi(line)
-      if err != nil{
-        fmt.Println("Error converting data to integers", err)
-      }
-      currentSum += calories
+    file, err := os.Open("input.txt")
+    if err != nil {
+        fmt.Println("Error opening the file:", err)
+        return
     }
-  }
+    defer file.Close()
 
-  if currentSum > maxCalories {
-    maxCalories = currentSum
-  }
-  fmt.Printf("The elf carrying the most calories was carrying %d calories.\n", maxCalories)
+    scanner := bufio.NewScanner(file)
+
+    currentSum := 0
+    sumOfCalories := []int{}
+
+    for scanner.Scan() {
+        line := scanner.Text()
+
+        if line == "" {
+            sumOfCalories = append(sumOfCalories, currentSum)
+            currentSum = 0
+        } else {
+            calories, err := strconv.Atoi(line)
+            if err != nil {
+                fmt.Println("Error converting data to integers:", err)
+                return
+            }
+            currentSum += calories
+        }
+    }
+
+    if currentSum > 0 {
+        sumOfCalories = append(sumOfCalories, currentSum)
+    }
+
+    sort.Sort(sort.Reverse(sort.IntSlice(sumOfCalories)))
+
+    totalSum := sumOfCalories[0] + sumOfCalories[1] + sumOfCalories[2]
+    fmt.Printf("The total calories carried by the top 3 Elves is: %d\n", totalSum)
 }
-
-
-
-// read the data line by line, if there's an empty line that is the end for that elf
-// if I run into an empty line, I should sum the total of the previous lines, and add
-// that total to a slice
-
-// At the end I should iterate through the slice and print the index + 1 with the highest sum
