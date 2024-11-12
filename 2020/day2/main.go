6faf65381808faf885166b1852ec4a16444614e9
0xGrizzly <gmcguir2@students.kennesaw.edu>
2024-11-12 16:19:36 -0500

    refactor of 1 star solution

--- 2020/day2/main.go
+++ 2020/day2/main.go
@@ -13,6 +13,7 @@ func main() {
 	if err != nil {
 		fmt.Println("Error opening the file", err)
 	}
+	defer file.Close()
 
 	validPasswords := 0
 
@@ -20,38 +21,24 @@ func main() {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		passwordSlice := strings.Split(line, " ")
 
-		minNumberOfInstances := 0
-		maxNumberOfInstances := 0
-		character := passwordSlice[1]
-		characterToSearchFor := strings.TrimRight(character, ":")[0]
+		parts := strings.Split(line, " ")
+		character := parts[1][0]
+		numberOfInstances := strings.Split(parts[0], "-")
+		minNumberOfInstances, _ := strconv.Atoi(numberOfInstances[0])
+		maxNumberOfInstances, _ := strconv.Atoi(numberOfInstances[1])
 
-		numberOfInstances := strings.Split(passwordSlice[0], "-")
-
-		minNumberOfInstances, err := strconv.Atoi(numberOfInstances[0])
-		if err != nil {
-			fmt.Println("Error converting minNumberOfInstances to int")
-		}
-
-		maxNumberOfInstances, err2 := strconv.Atoi(numberOfInstances[1])
-		if err2 != nil {
-			fmt.Println("Error convering maxNumberOfInstances to int")
-		}
-
-		passwordToSearch := passwordSlice[2]
+		passwordToSearch := parts[2]
 		numberOfOccurences := 0
 
 		for i := 0; i < len(passwordToSearch); i++ {
-			if passwordToSearch[i] == characterToSearchFor {
+			if passwordToSearch[i] == character {
 				numberOfOccurences++
 			}
 		}
 		if numberOfOccurences >= minNumberOfInstances && numberOfOccurences <= maxNumberOfInstances {
 			validPasswords++
 		}
-
 		fmt.Println(validPasswords)
-
 	}
 }
